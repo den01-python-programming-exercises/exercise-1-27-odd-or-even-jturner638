684b199d3c881d45963cccc6fdc771d753972a2f
Justin Turner <mrfathed@protonmail.com>
2021-12-04 11:45:33 -0600

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,11 @@
 def main():
     #write your code below this line
+    number = int(input("Give a number:"))
+
+    if number % 2 == 0:
+        print("Number {} is even.".format(number))
+    else:
+        print("Number {} is odd.".format(number))
 
 if __name__ == '__main__':
     main()
